5f9aa2b9c139c35b2976ed94c99ede2428226599
Alico_ <c.williams170@icloud.com>
2026-02-24 14:15:56 +0000

    paddles working

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -4,6 +4,7 @@ public class Script : MonoBehaviour
 {
     [SerializeField] Transform PaddleLeft;
     [SerializeField] Transform PaddleRight;
+    [SerializeField] float Speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     /*void Start()
     {
@@ -17,15 +18,31 @@ public class Script : MonoBehaviour
         {
             Debug.Log("aa");
             Vector3 newPosition = PaddleLeft.position;
-            newPosition.y -= 0.05f;
-            PaddleRight.position = newPosition;
+            newPosition.y -= Speed;
+            PaddleLeft.position = newPosition;
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             Debug.Log("aa");
+            Vector3 newPosition = PaddleLeft.position;
+            newPosition.y += Speed;
+            PaddleLeft.position = newPosition;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("aa");
             Vector3 newPosition = PaddleRight.position;
-            newPosition.y += 0.05f;
+            newPosition.y -= Speed;
+            PaddleRight.position = newPosition;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("aa");
+            Vector3 newPosition = PaddleRight.position;
+            newPosition.y += Speed;
             PaddleRight.position = newPosition;
         }
     }
