244dea4d2c51609361d89a46f0dd8612d3a36f27
Alico_ <c.williams170@icloud.com>
2026-02-24 14:05:52 +0000

    some scripting

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Script : MonoBehaviour
 {
+    [SerializeField] Transform PaddleLeft;
+    [SerializeField] Transform PaddleRight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /*void Start()
     {
-        
-    }
+
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("aa");
+            Vector3 newPosition = PaddleLeft.position;
+            newPosition.y -= 0.05f;
+            PaddleRight.position = newPosition;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("aa");
+            Vector3 newPosition = PaddleRight.position;
+            newPosition.y += 0.05f;
+            PaddleRight.position = newPosition;
+        }
     }
 }
